408ee4990787e79b3ab9c76897e40a8d8c0c5020
jundy.chow <zhoujundi711@163.com>
2017-12-11 14:02:03 +0800

    [init]增加企业的同步处理

--- src/main/java/com/fengchaoli/acenter/event/sync/enterprise/EnterpriseSyncEvent.java
+++ src/main/java/com/fengchaoli/acenter/event/sync/enterprise/EnterpriseSyncEvent.java
@@ -1,8 +1,6 @@
 package com.fengchaoli.acenter.event.sync.enterprise;
 
 import com.fengchaoli.acenter.model.Enterprise;
-import com.fengchaoli.acenter.model.User;
-import com.sun.tools.javac.comp.Enter;
 import lombok.Data;
 import org.springframework.context.ApplicationEvent;
 
