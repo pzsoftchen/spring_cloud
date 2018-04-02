52a7ff17bb7706ed10638fae4ec8ef99c9e0ba00
jundy.chow <zhoujundi711@163.com>
2018-04-02 14:03:50 +0800

    [init]提交跨域请求

--- gateway/src/main/java/com/fengchaoli/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/fengchaoli/gateway/GatewayApplication.java
@@ -3,6 +3,7 @@ package com.fengchaoli.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.cors.CorsConfiguration;
@@ -20,20 +21,23 @@ public class GatewayApplication {
 
 
 	@Bean
-	public CorsFilter corsFilter() {
+	public FilterRegistrationBean corsFilter() {
 		final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
 		final CorsConfiguration config = new CorsConfiguration();
-		config.setAllowCredentials(true);
-		config.addAllowedOrigin("*");
-		config.addAllowedHeader("*");
-		config.addAllowedMethod("OPTIONS");
-		config.addAllowedMethod("HEAD");
-		config.addAllowedMethod("GET");
-		config.addAllowedMethod("PUT");
-		config.addAllowedMethod("POST");
-		config.addAllowedMethod("DELETE");
-		config.addAllowedMethod("PATCH");
+		config.setAllowCredentials(true); // 允许cookies跨域
+		config.addAllowedOrigin("*");// #允许向该服务器提交请求的URI，*表示全部允许，在SpringMVC中，如果设成*，会自动转成当前请求头中的Origin
+		config.addAllowedHeader("*");// #允许访问的头信息,*表示全部
+		config.setMaxAge(1800L);// 预检请求的缓存时间（秒），即在这个时间段里，对于相同的跨域请求不会再预检了
+		config.addAllowedMethod("*");// 允许提交请求的方法，*表示全部允许
+     /*   config.addAllowedMethod("HEAD");
+        config.addAllowedMethod("GET");// 允许Get的请求方法
+        config.addAllowedMethod("PUT");
+        config.addAllowedMethod("POST");
+        config.addAllowedMethod("DELETE");
+        config.addAllowedMethod("PATCH");*/
 		source.registerCorsConfiguration("/**", config);
-		return new CorsFilter(source);
+		FilterRegistrationBean bean = new FilterRegistrationBean(new org.springframework.web.filter.CorsFilter(source));
+		bean.setOrder(0);
+		return bean;
 	}
 }
