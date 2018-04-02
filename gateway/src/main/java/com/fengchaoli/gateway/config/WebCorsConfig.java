d834aacc890619114364e3812dce7f0ff5c2a31c
jundy.chow <zhoujundi711@163.com>
2018-04-02 09:39:43 +0800

    [init]提交跨域请求

--- gateway/src/main/java/com/fengchaoli/gateway/config/WebCorsConfig.java
+++ gateway/src/main/java/com/fengchaoli/gateway/config/WebCorsConfig.java
@@ -13,21 +13,5 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 @Configuration
 public class WebCorsConfig {
 
-    @Bean
-    public CorsFilter corsFilter() {
-        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-        final CorsConfiguration config = new CorsConfiguration();
-        config.setAllowCredentials(true);
-        config.addAllowedOrigin("*");
-        config.addAllowedHeader("*");
-        config.addAllowedMethod("OPTIONS");
-        config.addAllowedMethod("HEAD");
-        config.addAllowedMethod("GET");
-        config.addAllowedMethod("PUT");
-        config.addAllowedMethod("POST");
-        config.addAllowedMethod("DELETE");
-        config.addAllowedMethod("PATCH");
-        source.registerCorsConfiguration("/**", config);
-        return new CorsFilter(source);
-    }
+
 }
