e9ba3fe3fdc5c8bae3ecba842dff959bda7f1dbd
jundy.chow <zhoujundi711@163.com>
2018-04-05 12:34:48 +0800

    [init]修改gradle的启动文件

--- create_service.sh
+++ create_service.sh
@@ -1,32 +1,40 @@
-docker network create -d overlay --subnet=192.168.66.0/24 springcloud
+docker network create -d overlay --subnet=192.168.66.0/24 fengchaoli
 
-#discovery
-docker service create --with-registry-auth --network springcloud \
---name discovery --log-opt max-size=100m --log-opt max-file=3 \
--p 8761:80 \
-registry.cn-shenzhen.aliyuncs.com/nothing/nothing-discovery:1.0
+#config
+docker service create --with-registry-auth --network fengchaoli \
+--name config --log-opt max-size=100m --log-opt max-file=3 \
+registry-internal.cn-shenzhen.aliyuncs.com/dev_new_fengchaoli/config:latest
 
+#registry
+docker service create --with-registry-auth --network fengchaoli \
+--name registry --log-opt max-size=100m --log-opt max-file=3 \
+registry-internal.cn-shenzhen.aliyuncs.com/dev_new_fengchaoli/registry:latest
 
-#chain
-docker service create --with-registry-auth --network springcloud \
---name chain --log-opt max-size=100m --log-opt max-file=3 \
--p 8411:80 \
-registry.cn-shenzhen.aliyuncs.com/nothing/nothing-chain:1.0
+#zipkin
+docker service create --with-registry-auth --network fengchaoli \
+--name zipkin --log-opt max-size=100m --log-opt max-file=3 \
+registry-internal.cn-shenzhen.aliyuncs.com/dev_new_fengchaoli/zipkin:latest
 
+#monitor
+docker service create --with-registry-auth --network fengchaoli \
+--name monitor --log-opt max-size=100m --log-opt max-file=3 \
+registry-internal.cn-shenzhen.aliyuncs.com/dev_new_fengchaoli/monitor:latest
 
 #geteway
 docker service create --with-registry-auth --network fengchaoli \
 --name gateway --log-opt max-size=100m --log-opt max-file=3 \
-registry-internal.cn-shenzhen.aliyuncs.com/dev_new_fengchaoli/gateway:1.0
-
+registry-internal.cn-shenzhen.aliyuncs.com/dev_new_fengchaoli/gateway:latest
 
 #ucenter
 docker service create --with-registry-auth --network fengchaoli \
 --name ucenter --log-opt max-size=100m --log-opt max-file=3 \
-registry-internal.cn-shenzhen.aliyuncs.com/dev_new_fengchaoli/ucenter:1.0
+registry-internal.cn-shenzhen.aliyuncs.com/dev_new_fengchaoli/ucenter:latest
 
 
-#backend
-docker service create --with-registry-auth --network springcloud \
---name backend --log-opt max-size=100m --log-opt max-file=3 \
-registry.cn-shenzhen.aliyuncs.com/nothing/nothing-backend:1.0
+
+
+#monitor
+docker service create --with-registry-auth --network fengchaoli \
+--name monitor --log-opt max-size=100m --log-opt max-file=3 \
+-p 8761:80 \
+registry-internal.cn-shenzhen.aliyuncs.com/dev_new_fengchaoli/monitor:latest
