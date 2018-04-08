fd15cca27666fc9ace64c5aaee2e93ac50a9e368
jundy.chow <zhoujundi711@163.com>
2018-04-08 15:10:54 +0800

    [init]修改gradle的启动文件

--- zipkin/src/main/java/com/fengchaoli/config/ZipkinApplication.java
+++ zipkin/src/main/java/com/fengchaoli/config/ZipkinApplication.java
@@ -3,11 +3,13 @@ package com.fengchaoli.config;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
+import zipkin.server.EnableZipkinServer;
+//import org.springframework.cloud.sleuth.zipkin.stream.EnableZipkinStreamServer;
 
 @SpringBootApplication
-@EnableZipkinStreamServer
+//@EnableZipkinStreamServer
 @EnableDiscoveryClient
+@EnableZipkinServer
 public class ZipkinApplication {
 
 	public static void main(String[] args) {
