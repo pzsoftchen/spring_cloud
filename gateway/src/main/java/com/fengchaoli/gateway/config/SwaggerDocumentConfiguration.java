eab5bcc0793779d5c6801bb0e6282df68e406f8b
jundy.chow <zhoujundi711@163.com>
2018-04-10 11:52:16 +0800

    [init]修改回steam的形式

--- gateway/src/main/java/com/fengchaoli/gateway/config/SwaggerDocumentConfiguration.java
+++ gateway/src/main/java/com/fengchaoli/gateway/config/SwaggerDocumentConfiguration.java
@@ -15,13 +15,14 @@ public class SwaggerDocumentConfiguration implements SwaggerResourcesProvider {
     @Override
     public List<SwaggerResource> get() {
         List resources = new ArrayList<>();
-        resources.add(swaggerResource("认证中心", "/uaa/v2/api-docs", "2.0"));
-        resources.add(swaggerResource("用户中心", "/uc/v2/api-docs", "2.0"));
-        resources.add(swaggerResource("权限系统", "/upm/v2/api-docs", "2.0"));
-        resources.add(swaggerResource("组织架构", "/organization/v2/api-docs", "2.0"));
-        resources.add(swaggerResource("消息中心", "/message/v2/api-docs", "2.0"));
-        resources.add(swaggerResource("通知中心", "/notification/v2/api-docs", "2.0"));
-        resources.add(swaggerResource("公共服务", "/common/v2/api-docs", "2.0"));
+        resources.add(swaggerResource("e-fcl", "/fcl/rest/v2/api-docs?group=api", "2.0"));
+//        resources.add(swaggerResource("认证中心", "/uaa/v2/api-docs", "2.0"));
+//        resources.add(swaggerResource("用户中心", "/uc/v2/api-docs", "2.0"));
+//        resources.add(swaggerResource("权限系统", "/upm/v2/api-docs", "2.0"));
+//        resources.add(swaggerResource("组织架构", "/organization/v2/api-docs", "2.0"));
+//        resources.add(swaggerResource("消息中心", "/message/v2/api-docs", "2.0"));
+//        resources.add(swaggerResource("通知中心", "/notification/v2/api-docs", "2.0"));
+//        resources.add(swaggerResource("公共服务", "/common/v2/api-docs", "2.0"));
         return resources;
     }
 
