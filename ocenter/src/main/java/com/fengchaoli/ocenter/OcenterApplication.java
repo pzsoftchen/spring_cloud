d86f5687224da4c990851f3bd22fa0afed6da686
jundy.chow <zhoujundi711@163.com>
2018-05-15 15:47:24 +0800

    [init]一次大的调整

--- ocenter/src/main/java/com/fengchaoli/ocenter/OcenterApplication.java
+++ ocenter/src/main/java/com/fengchaoli/ocenter/OcenterApplication.java
@@ -4,9 +4,11 @@ import com.fengchaoli.basic.oauth2.feign.EnableOAuth2ClientFeign;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.EnableLoadTimeWeaving;
 import org.springframework.context.annotation.aspectj.EnableSpringConfigured;
+import org.springframework.instrument.classloading.InstrumentationLoadTimeWeaver;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableOAuth2Client;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
@@ -31,5 +33,11 @@ public class OcenterApplication extends ResourceServerConfigurerAdapter {
 		http.authorizeRequests()
 				.antMatchers("/v2/api-docs").permitAll()
 				.and().authorizeRequests().anyRequest().authenticated();
+	}
+
+	@Bean
+	public InstrumentationLoadTimeWeaver loadTimeWeaver()  throws Throwable {
+		InstrumentationLoadTimeWeaver loadTimeWeaver = new InstrumentationLoadTimeWeaver();
+		return loadTimeWeaver;
 	}
 }
